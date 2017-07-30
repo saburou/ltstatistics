5ae0f2bd32d8248e7b8b3c654ad09f0f6852f8bf
saburou <saburou@users.noreply.github.com>
2017-07-30 12:45:02 +0900

    Refactor staticmethods and database path.

--- core/register.py
+++ core/register.py
@@ -2,10 +2,7 @@
 from abc import ABCMeta, abstractmethod
 from datetime import datetime
 from model.models import Stream, StTotalView, StView, StComment
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-
-Engine = create_engine('sqlite:///database.sqlite3', echo=True)
+from core.session import DBSessionFactory
 
 
 class Register(metaclass=ABCMeta):
@@ -27,8 +24,7 @@ class ViewRegister(Register):
     Regist stream viewing information into database.
     """
     def regist(self, data):
-        _SESSION = sessionmaker(bind=Engine)
-        session = _SESSION()
+        session = DBSessionFactory.create()
 
         currentdate = datetime.now()
         total = 0
@@ -37,20 +33,20 @@ class ViewRegister(Register):
                 continue
             # Regist viewing.
             view = StView(id=d.id, date=currentdate, amount=d.viewing)
-            if view.count(StView, d.id, currentdate, session) > 0:
-                view.update(StView, d.id, currentdate, d.viewing, session)
+            if view.count(d.id, currentdate, session) > 0:
+                view.update(d.id, currentdate, d.viewing, session)
             else:
-                view.regist(StView, d.id, currentdate, d.viewing, session)
+                view.regist(d.id, currentdate, d.viewing, session)
             total += int(d.viewing)
             # Regist comments.
             comment = StComment(id=d.id, date=currentdate, amount=d.comments)
-            if comment.count(StComment, d.id, currentdate, session) > 0:
-                comment.update(StComment, d.id, currentdate, d.comments, session)
+            if comment.count(d.id, currentdate, session) > 0:
+                comment.update(d.id, currentdate, d.comments, session)
             else:
-                comment.regist(StComment, d.id, currentdate, d.comments, session)
+                comment.regist(d.id, currentdate, d.comments, session)
         # Regist total stream view.
         totalview = StTotalView(date=currentdate, amount=total)
-        totalview.regist(StTotalView, currentdate, total, session)
+        totalview.regist(currentdate, total, session)
 
         session.commit()
         session.close()
@@ -61,13 +57,12 @@ class StreamRegister(Register):
     Regist stream information.
     """
     def regist(self, data):
-        _SESSION = sessionmaker(bind=Engine)
-        session = _SESSION()
+        session = DBSessionFactory.create()
         for s in data:
             if s is None:
                 continue
-            if Stream.count(Stream, target_id=s.id, session=session) == 0:
-                Stream.regist(Stream, target_id=s.id, target_title=s.title, target_author=s.author,
+            if Stream.count(target_id=s.id, session=session) == 0:
+                Stream.regist(target_id=s.id, target_title=s.title, target_author=s.author,
                               target_startdate=s.startdate, session=session)
 
         session.commit()
--- model/models.py
+++ model/models.py
@@ -15,19 +15,19 @@ class StComment(Base):
         return "<StComment('%s', '%s', '%s')>" % (self.id, self.date, self.amount)
 
     @staticmethod
-    def select(cls, target_id, session):
+    def select(target_id, session):
         return session.query(StComment).filter(StComment.id == target_id).all()
 
     @staticmethod
-    def count(cls, target_id, target_date, session):
+    def count(target_id, target_date, session):
         return session.query(StComment).filter(StComment.id == target_id, StComment.date == target_date).count()
 
     @staticmethod
-    def regist(cls, target_id, target_date, target_amount, session):
+    def regist(target_id, target_date, target_amount, session):
         session.add(StComment(id=target_id, date=target_date, amount=target_amount))
 
     @staticmethod
-    def update(cls, target_id, target_date, target_amount, session):
+    def update(target_id, target_date, target_amount, session):
         session.add(StComment(id=target_id, date=target_date, amount=target_amount))
 
 
@@ -41,19 +41,19 @@ class StTotalView(Base):
         return "<StTotalView('%s','%s')>" % (self.date, self.amount)
 
     @staticmethod
-    def select(cls, target_date, session):
-        return session.query(StTotalView).filter(StTotalView.date == target_date).all()
+    def select(target_startdate, target_enddate, session):
+        return session.query(StTotalView).filter(StTotalView.date.between(target_startdate, target_enddate)).all()
 
     @staticmethod
-    def count(cls, target_date, session):
+    def count(target_date, session):
         return session.query(StTotalView).filter(StTotalView.date == target_date).count()
 
     @staticmethod
-    def regist(cls, target_date, target_amount, session):
+    def regist(target_date, target_amount, session):
         session.add(StTotalView(date=target_date, amount=target_amount))
 
     @staticmethod
-    def update(cls, target_date, target_amount, session):
+    def update(target_date, target_amount, session):
         session.add(StTotalView(date=target_date, amount=target_amount))
 
 
@@ -68,19 +68,19 @@ class StView(Base):
         return "<StView('%s', '%s', '%s')>" % (self.id, self.measuredate, self.amount)
 
     @staticmethod
-    def count(cls, target_id, target_date, session):
+    def count(target_id, target_date, session):
         return session.query(StView).filter(StView.id == target_id, StView.date == target_date).count()
 
     @staticmethod
-    def select(cls, target_id, session):
+    def select(target_id, session):
         return session.query(StView).filter(StView.id == target_id).all()
 
     @staticmethod
-    def regist(cls, target_id, target_date, target_amount, session):
+    def regist(target_id, target_date, target_amount, session):
         session.add(StView(id=target_id, date=target_date, amount=target_amount))
 
     @staticmethod
-    def update(cls, target_id, target_date, target_amount, session):
+    def update(target_id, target_date, target_amount, session):
         session.add(StView(id=target_id, date=target_date, amount=target_amount))
 
 
@@ -98,23 +98,23 @@ class Stream(Base):
         return "<Stream('%s', '%s', '%s', '%s')>" % (self.id, self.title, self.author, self.startdate)
 
     @staticmethod
-    def select(cls, target_id, session):
+    def select(target_id, session):
         return session.query(Stream).filter(Stream.id == target_id).all()
 
     @staticmethod
-    def count(cls, target_id, session):
+    def count(target_id, session):
         return session.query(Stream).filter(Stream.id == target_id).count()
 
     @staticmethod
-    def regist(cls, target_id, target_title, target_author, target_startdate, session):
+    def regist(target_id, target_title, target_author, target_startdate, session):
         session.add(Stream(id=target_id, title=target_title, author=target_author, startdate=target_startdate))
 
     @staticmethod
-    def update(cls, target_id, target_title, target_author, target_startdate, session):
+    def update(target_id, target_title, target_author, target_startdate, session):
         # session.add(Stream(id=target_id, title=target_title, author=target_author, startdate=target_startdate))
         raise NotImplementedError
 
 if __name__ == "__main__":
-    schema = 'sqlite:///../core/database.sqlite3'
+    schema = 'sqlite:///../database.sqlite3'
     engine = create_engine(schema, echo=True)
     Base.metadata.create_all(engine)
