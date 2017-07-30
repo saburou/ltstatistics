3268dbb124752f43a2b40592d6a529d9bf149aa5
saburou <saburou@users.noreply.github.com>
2017-07-30 12:46:32 +0900

    Remove alpha methods.
    Refactor method.

--- index.py
+++ index.py
@@ -2,10 +2,11 @@
 # -*- coding: utf-8 -*-
 from bottle import get, run, response
 import json
-from entity.st_total_view import StTotalView
-from entity.st_comment import StComment
-from common.validator.datevalidator import *
-from common.message.message import Message
+from model.models import StTotalView
+from common.validator import DayValidator
+from common.message import Message
+from common.date import DateUtils
+from core.session import DBSessionFactory
 
 CONTENT_TYPE = 'application/json'
 
@@ -15,85 +16,29 @@ def daytotalview(date):
     """
     指定年月日の統計視聴情報を返します。
     :param date: 対象日
-    :return: 統計視聴情報 
+    :return: 統計視聴情報
     """
     if not DayValidator().isvalid(date):
         return Message.error(Message.ID_BAD_REQUEST, Message.TEXT_INVALID_DATE)
 
-    dateparams = date.split("-")
-    year = dateparams[0]
-    month = dateparams[1]
-    day = dateparams[2]
-
     response.content_type = CONTENT_TYPE
-    data = StTotalView().findday('%s-%s-%s' % (year, month, day), '%s-%s-%s' % (year, month, str(int(day)+1)))
+
+    startdate = DateUtils.parse(date)
+    enddate = DateUtils.add_day(startdate, 1)
+
+    session = DBSessionFactory.create()
+    data = StTotalView().select(target_startdate='%s-%s-%s' % (startdate.year, startdate.month, startdate.day),
+                                target_enddate='%s-%s-%s' % (enddate.year, enddate.month, enddate.day),
+                                session=session)
+
     result = []
     for d in data:
-        result.append({'date': d.get_measuredate(), 'amount': d.get_amount()})
+        result.append({'date': d.date.strftime("%Y/%m/%d %H:%M:%S"), 'amount': d.amount})
+
+    session.close()
+
     return json.dumps({'unit': 'day', 'view': result}).replace("\\", "")
 
-@get('/api/totalcomment/day/<date>')
-def daytotalcomment(date):
-    """
-    指定年月日のコメント情報を返します。
-    :param date: 対象日
-    :return: 統計視聴情報 
-    """
-    if not DayValidator().isvalid(date):
-        return Message.error(Message.ID_BAD_REQUEST, Message.TEXT_INVALID_DATE)
 
-    dateparams = date.split("-")
-    year = dateparams[0]
-    month = dateparams[1]
-    day = dateparams[2]
-
-    response.content_type = CONTENT_TYPE
-    data = StComment().findday('%s-%s-%s' % (year, month, day), '%s-%s-%s' % (year, month, str(int(day)+1)))
-    result = []
-    for d in data:
-        result.append({'date': d.get_measuredate(), 'amount': d.get_amount()})
-    return json.dumps({'unit': 'day', 'comment': result}).replace("\\", "")
-
-@get('/api/totalview/month/<date>')
-def monthtotalview(date):
-    """
-    指定年月の統計視聴情報を返します。 
-    :param date: 対象月
-    :return: 統計視聴情報 
-    """
-    if not MonthValidator().isvalid(date):
-        return Message.error(Message.ID_BAD_REQUEST, Message.TEXT_INVALID_DATE)
-
-    dateparams = date.split("-")
-    year = dateparams[0]
-    month = dateparams[1]
-
-    response.content_type = CONTENT_TYPE
-    data = StTotalView().findmonth('%s-%s' % (year, month), '%s-%s' % (year, str(int(month)+1)))
-    result = []
-    for d in data:
-        result.append({'date': d.get_measuredate(), 'amount': d.get_amount()})
-    return json.dumps({'unit': 'month', 'view': result}).replace("\\", "")
-
-
-@get('/api/totalview/year/<date>')
-def yeartotalview(date):
-    """
-    指定年の統計視聴情報を返します。
-    :param year: 対象年
-    :return: 統計視聴情報 
-    """
-    if not YearValidator().isvalid(date):
-        return Message.error(Message.ID_BAD_REQUEST, Message.TEXT_INVALID_DATE)
-
-    dateparams = date.split("-")
-    year = dateparams[0]
-
-    response.content_type = CONTENT_TYPE
-    data = StTotalView().findyear('%s' % year, '%s' % str(int(year)+1))
-    result = []
-    for d in data:
-        result.append({'date': d.get_measuredate(), 'amount': d.get_amount()})
-    return json.dumps({'unit': 'year', 'view': result}).replace("\\", "")
-
-run(host='localhost', port=8080)
+if __name__ == "__main__":
+    run(host='localhost', port=8080)
